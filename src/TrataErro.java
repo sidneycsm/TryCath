66c6f85c9d4e038e00477932fc75256504f0a038
sidneycsm <sidneycsm@gmail.com>
2019-03-16 13:04:12 -0300

    Lancando new Exception

---
+++ src/TrataErro.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TrataErro {
+	
+	public void dividir() {
+	
+		try {dividindo ((new Scanner scanner = new Scanner(System.in);
+		String xnumeroDivide = Integer.valueOf(scanner.next());
+		string numeroDivide = Integer.valueOf(scanner.next());
+		
+		dividindo(numeroDivide);
+		
+	}
+	
+	public void dividindo(int divisor) {
+		
+		
+		return 10 / divisor;
+
+		
+	}
+
+}
